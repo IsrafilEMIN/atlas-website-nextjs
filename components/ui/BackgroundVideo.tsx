56b6f75c29dbc4f2cb2be4299b1fe09b7a25eca7
IsrafilEMIN <98862614+IsrafilEMIN@users.noreply.github.com>
2025-02-21 10:56:34 -0500

    Update BackgroundVideo.tsx

--- components/ui/BackgroundVideo.tsx
+++ components/ui/BackgroundVideo.tsx
@@ -1,12 +1,23 @@
 "use client";
+import { useEffect, useRef } from "react";
 
 export default function BackgroundVideo() {
+    const videoRef = useRef<HTMLVideoElement>(null);
+
+  useEffect(() => {
+    if (videoRef.current) {
+      videoRef.current.play().catch((error) => console.log("Auto-play failed:", error));
+    }
+  }, [])
+
     return (
         <video
+            ref={videoRef}
             autoPlay
             loop
             muted
             playsInline
+            preload="auto"
             className="object-cover w-full h-full"
         >
             <source src="/assets/hero-background.mp4" type="video/mp4" />
