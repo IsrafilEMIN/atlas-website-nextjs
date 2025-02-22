c86fd2e2e6ee291bd0363cb5c31c2221f56ff840
Kutluk <iso.general@icloud.com>
2025-02-21 19:28:48 -0500

    fixed iphone safari issues

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 // pages/_app.tsx
+import Head from "next/head";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "@/lib/queryClient";
 import { Toaster } from "@/components/ui/toaster";
@@ -8,16 +9,20 @@ import "@/styles/globals.css"; // Ensure global styles are included
 import { AppProps } from "next/app";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <QueryClientProvider client={queryClient}>
-      <div className="min-h-screen flex flex-col">
-        <Header />
-        <div className="flex-grow">
-          <Component {...pageProps} />
-        </div>
-        <Footer />
-      </div>
-      <Toaster />
-    </QueryClientProvider>
-  );
+    return (
+        <QueryClientProvider client={queryClient}>
+            <Head>
+                <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, viewport-fit=cover" />
+                <meta name="apple-mobile-web-app-capable" content="yes" />
+            </Head>
+            <div className="min-h-screen flex flex-col">
+                <Header />
+                <div className="flex-grow">
+                    <Component {...pageProps} />
+                </div>
+                <Footer />
+            </div>
+            <Toaster />
+        </QueryClientProvider>
+    );
 }
