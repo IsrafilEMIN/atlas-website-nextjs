19098e5d88762889361407940f22e2f2fdde1abf
Kutluk <iso.general@icloud.com>
2025-02-22 16:36:17 -0500

    fixed hydration issues

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,47 @@
-import Hero from "@/components/home/Hero";
-import Features from "@/components/home/Features";
-import ProductShowcase from "@/components/home/ProductShowcase";
-import Testimonials from "@/components/home/Testimonials";
+
+import * as React from "react";
 import dynamic from "next/dynamic";
 
-const Header = dynamic(() => import("../components/layout/Header"), {
-  ssr: false,
+const Hero = dynamic(() => import("@/components/home/Hero"), {
+    ssr: true,
+});
+
+const Features = dynamic(() => import("@/components/home/Features"), {
+    ssr: true,
+});
+
+const ProductShowcase = dynamic(() => import("@/components/home/ProductShowcase"), {
+    ssr: true,
+});
+
+const Testimonials = dynamic(() => import("@/components/home/Testimonials"), {
+    ssr: true,
+});
+
+const Header = dynamic(() => import("@/components/layout/Header"), {
+    ssr: false,
 });
 
 export default function Home() {
-  return (
-    <div className="bg-white min-h-screen">
-      <Header />
-      <main>
-        <Hero />
-        <Features />
-        <Testimonials />
-        <ProductShowcase />
-      </main>
-    </div>
-  );
+    const [mounted, setMounted] = React.useState(false);
+
+    React.useEffect(() => {
+        setMounted(true);
+    }, []);
+
+    if (!mounted) {
+        return null;
+    }
+
+    return (
+        <div className="bg-white min-h-screen">
+            <Header />
+            <main className="w-full">
+                <Hero />
+                <Features />
+                <Testimonials />
+                <ProductShowcase />
+            </main>
+        </div>
+    );
 }
