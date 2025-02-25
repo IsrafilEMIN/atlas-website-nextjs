f8e5e87568404835b3c1854488b79155848e84a7
Kutluk <iso.general@icloud.com>
2025-02-24 22:16:42 -0500

    fixed favicon more

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -13,8 +13,12 @@ export default function MyApp({ Component, pageProps }: AppProps) {
         <QueryClientProvider client={queryClient}>
             <Head>
                 {/* Fallback Favicon for browsers that don't support `prefers-color-scheme` */}
-                <link rel="icon" href="/assets/favicon-dark.ico" type="image/x-icon"/>
-                <link rel="icon" href="/assets/favicon-dark.png" type="image/png"/>
+                <link rel="icon" type="image/png" href="/assets/favicon-96x96.png" sizes="96x96" />
+                <link rel="icon" type="image/svg+xml" href="/assets/favicon.svg" />
+                <link rel="shortcut icon" href="/assets/favicon.ico" />
+                <link rel="apple-touch-icon" sizes="180x180" href="/assets/apple-touch-icon.png" />
+                <meta name="apple-mobile-web-app-title" content="Atlas Painting" />
+                <link rel="manifest" href="/assets/site.webmanifest" />
 
                 <meta name="viewport"
                       content="width=device-width, initial-scale=1, maximum-scale=1, viewport-fit=cover"/>
