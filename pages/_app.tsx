e8539be129c9842bf9ddef6289c8c9e940ab9af1
IsrafilEMIN <98862614+IsrafilEMIN@users.noreply.github.com>
2025-02-24 21:22:22 -0500

    Update _app.tsx
    Added apple icon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -13,13 +13,11 @@ export default function MyApp({ Component, pageProps }: AppProps) {
         <QueryClientProvider client={queryClient}>
             <Head>
                 {/* Fallback Favicon for browsers that don't support `prefers-color-scheme` */}
-                <link rel="icon" href="/assets/favicon-dark.ico" type="image/x-icon"/>
-                <link rel="icon" href="/assets/favicon-dark.png" type="image/png"/>
-
-                <meta name="viewport"
-                      content="width=device-width, initial-scale=1, maximum-scale=1, viewport-fit=cover"/>
-                <meta name="apple-mobile-web-app-capable" content="yes"/>
-            </Head>
+                <link rel="icon" href="/assets/favicon-dark.ico" type="image/x-icon" />
+                <link rel="apple-touch-icon" href="/assets/favicon-dark.png" />
+                <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, viewport-fit=cover" />
+                <meta name="apple-mobile-web-app-capable" content="yes" />
+            ​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​</Head>
             <div className="min-h-screen flex flex-col">
                 <Header/>
                 <div className="flex-grow">
